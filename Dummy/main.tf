dcc2c8c0e1e8bab42f892cc4ea0cb2d78c058cc1
rupeshvarma100 <rupeshvarma100@gmail.com>
2025-12-17 11:23:15 +0530

    update dummy folder

--- Dummy/main.tf
+++ Dummy/main.tf
@@ -10,29 +10,30 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = "e91c9d5f-a463-4ecd-887a-ae2efce577bd"
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources1"
-  location = "EAST US"
+  name     = "terraform-RG"
+  location = "Central India"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = "terraform-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "internal"
+  name                 = "terraform-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
+  name                = "terrfaorm-nic"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
@@ -40,12 +41,11 @@ resource "azurerm_network_interface" "example" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-machine"
+  name                = "terraform-vm"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_B1s"
@@ -56,7 +56,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("C:/Users/rupes/.ssh/id_rsa.pub")
   }
 
   os_disk {
