b0c2dc7433353a41777a4fcba961e9dcf36b03b6
Rupesh Varma <41001993+rupeshvarma100@users.noreply.github.com>
2025-12-18 23:34:05 +0530

    Update backend.tf

--- Dummy/backend.tf
+++ Dummy/backend.tf
@@ -4,6 +4,7 @@ terraform {
     storage_account_name = "rvkeyterraform"
     container_name = "terraform-backend"
     key = "terraform.tfstate"
-    access_key = "UvUpiPnESSZW3A/uOah/+w97MwZ8QtaFHKgxAd9DyCoxw7dlwjNNgYuwNCMqYe9qHqKrzi/2vkol+AStD0o2Eg=="
+    access_key = "Add Access Key from the Blob Storage"
   }
+
 }
